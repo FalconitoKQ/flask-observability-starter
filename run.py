f69f5cbddb780dc976c80fe1781df02a6d737920
Lukasz <lukasz.klak2003@gmail.com>
2026-01-08 22:04:33 +0100

    add prometheus-client in run.py

--- run.py
+++ run.py
@@ -1,13 +1,32 @@
-from flask import render_template, Flask
+from flask import render_template, Flask, request, Response
+from prometheus_client import Counter, generate_latest, CONTENT_TYPE_LATEST
 
 app = Flask(__name__)
-@app.route('/')
+
+#
+REQUESTS_TOTAL = Counter(
+    "http_requests_total",
+    "Total HTTP requests",
+    ["method", "endpoint"]
+)
+
+@app.get('/')
 def index():
     return "Hello World"
 
-@app.route('/healthy')
+@app.get('/healthy')
 def healthy():
     return {'status': 'enabled'}
+
+@app.get('/metrics')
+def metrics():
+    return Response(generate_latest(), content_type=CONTENT_TYPE_LATEST)
+
+@app.before_request
+def count_requests():
+    ep = request.path or "unknown"
+    REQUESTS_TOTAL.labels(request.method, ep).inc()
+
 
 if __name__ == '__main__':
     app.run(debug=True)
